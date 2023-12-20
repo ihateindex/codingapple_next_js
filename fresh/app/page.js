e2fbc3e0761c25f7b721f8353cab681c2ba79cf4
ihateindex <47144148+ihateindex@users.noreply.github.com>
2023-12-20 17:42:01 +0900

    feat: 레이아웃,라우팅 기능 추가
    1. /list 페이지 추가
    2. /cart, /cart/payment 페이지 추가
    3. layout에 nav bar 추가

---
+++ fresh/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    let name = 'ihateindex';
+    return (
+        <div>
+            <h4 className="title">애플후레시</h4>
+            <p className="title-sub">by {name}</p>
+        </div>
+    );
+}
