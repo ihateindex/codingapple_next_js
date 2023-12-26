89342bbbcb3921b99e6695114b459853e49f25da
ihateindex <47144148+ihateindex@users.noreply.github.com>
2023-12-26 17:49:43 +0900

    feat: useState 추가

--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -1,5 +1,10 @@
+'use client';
+
+import { useState } from 'react';
+
 export default function List() {
     let 상품 = ['Tomatoes', 'Pasta', 'Coconut'];
+    let [수량, 수량변경] = useState([0, 0, 0]);
     return (
         <div>
             <h4 className="title">상품목록</h4>
@@ -8,6 +13,29 @@ export default function List() {
                     <div className="food" key={index}>
                         <img src={`/food${index}.png`} alt="" className="food-img" />
                         <h4>{item} $40</h4>
+                        <button
+                            onClick={() => {
+                                수량변경((prevState) => {
+                                    let copy = [...prevState];
+                                    copy[index]--;
+                                    return copy;
+                                });
+                            }}
+                        >
+                            -
+                        </button>
+                        <span> {수량[index]} </span>
+                        <button
+                            onClick={() => {
+                                수량변경((prevState) => {
+                                    let copy = [...prevState];
+                                    copy[index]++;
+                                    return copy;
+                                });
+                            }}
+                        >
+                            +
+                        </button>
                     </div>
                 );
             })}
