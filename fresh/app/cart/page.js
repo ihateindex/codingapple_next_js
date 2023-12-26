693c63ead3f37bc1138c93edae30a01fc4a93e8c
ihateindex <47144148+ihateindex@users.noreply.github.com>
2023-12-26 16:17:27 +0900

    feat: props 추가

--- fresh/app/cart/page.js
+++ fresh/app/cart/page.js
@@ -1,17 +1,20 @@
 export default function Cart() {
+    let 장바구니 = ['토마토', '파스타'];
     return (
         <div>
             <h4 className="title">Cart</h4>
-            <div className="cart-item">
-                <p>상품명</p>
-                <p>$40</p>
-                <p>1개</p>
-            </div>
-            <div className="cart-item">
-                <p>상품명</p>
-                <p>$40</p>
-                <p>1개</p>
-            </div>
+            <CartItem item={장바구니[0]}></CartItem>
+            <CartItem item={장바구니[1]}></CartItem>
+        </div>
+    );
+}
+
+function CartItem(props) {
+    return (
+        <div className="cart-item">
+            <p>{props.item}</p>
+            <p>$40</p>
+            <p>1개</p>
         </div>
     );
 }
