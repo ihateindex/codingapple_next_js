f0c6e6b53d2387ae79972c84bbffd8160ad0ab99
ihateindex <47144148+ihateindex@users.noreply.github.com>
2024-01-21 00:45:03 +0900

    feat: list 페이지 다이나믹 렌더링 페이지로 변경
    1. list 페이지는 Listitem 컴포넌트의 부모로써 Listitem 컴포넌트는 클라이언트 컴포넌트로 db데이터를 map을 사용해 동적으로 뿌려주고 있음
    2. 하지만 build시 부모인 list 페이지가 다이나믹 렌더링(동적)이 아닌 스테틱이라 페이지에 반영되지않음
    3. 이를 해결하기 위해 list 페이지를 동적 페이지로 변경

--- forum/app/list/page.js
+++ forum/app/list/page.js
@@ -1,6 +1,9 @@
 import { connectDB } from '@/util/database.js';
 // import DetailLink from './DetailLink';
 import ListItem from './ListItem';
+
+export const dynamic = 'force-dynamic';
+
 export default async function List() {
     const db = (await connectDB).db('forum');
     let result = await db.collection('post').find().toArray();
