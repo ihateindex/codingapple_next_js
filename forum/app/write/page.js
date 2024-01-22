c6728743d34147d946b9e198473d94b16142e29e
ihateindex <47144148+ihateindex@users.noreply.github.com>
2024-01-22 11:19:50 +0900

    feat: 아이디, 비밀번호 방식 로그인 기능 추가

--- forum/app/write/page.js
+++ forum/app/write/page.js
@@ -1,12 +1,20 @@
-export default function Write() {
-    return (
-        <div className="p-20">
-            <h4>글작성</h4>
-            <form action="/api/post/new" method="POST">
-                <input type="text" name="title" placeholder="글제목" />
-                <input type="text" name="content" placeholder="글내용" />
-                <button type="submit">등록</button>
-            </form>
-        </div>
-    );
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import { getServerSession } from 'next-auth';
+
+export default async function Write() {
+    let session = await getServerSession(authOptions);
+    if (!session) {
+        return <div>로그인하세요</div>;
+    } else {
+        return (
+            <div className="p-20">
+                <h4>글작성</h4>
+                <form action="/api/post/new" method="POST">
+                    <input type="text" name="title" placeholder="글제목" />
+                    <input type="text" name="content" placeholder="글내용" />
+                    <button type="submit">등록</button>
+                </form>
+            </div>
+        );
+    }
 }
--- forum/pages/api/post/delete.js
+++ forum/pages/api/post/delete.js
@@ -19,7 +19,7 @@ export default async function handler(요청, 응답) {
             let result = await db.collection('post').deleteOne({ _id: new ObjectId(_id), author: session.user.email });
             // console.log(result);
             if (!result.deletedCount) {
-                return 응답.status(500).json('삭제 실패');
+                return 응답.status(500).json('삭제실패');
             }
             return 응답.status(200).json('삭제완료');
         } catch (error) {
