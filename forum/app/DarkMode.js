99ebc652bb10f565c0bd907c20e7318bda3165c4
ihateindex <47144148+ihateindex@users.noreply.github.com>
2024-02-09 02:12:14 +0900

    feat: 라이트/다크모드시 모드 아이콘 전환 기능 추가

--- forum/app/DarkMode.js
+++ forum/app/DarkMode.js
@@ -1,15 +1,18 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 export default function DarkMode() {
+    const [mode, setMode] = useState('light');
+    const [cookie, setCookie] = useState('');
     let router = useRouter();
     useEffect(() => {
-        let 쿠키값 = ('; ' + document.cookie).split(`; mode=`).pop().split(';')[0];
-        if (쿠키값 == '') {
+        setCookie(('; ' + document.cookie).split(`; mode=`).pop().split(';')[0]);
+        if (cookie == '') {
             document.cookie = 'mode=light; max-age=' + 3600 * 24 * 400;
         }
+        setMode(mode);
     }, []);
     return (
         <span
@@ -17,14 +20,15 @@ export default function DarkMode() {
                 let 쿠키값 = ('; ' + document.cookie).split(`; mode=`).pop().split(';')[0];
                 if (쿠키값 == 'light') {
                     document.cookie = 'mode=dark; max-age=' + 3600 * 24 * 400;
+                    setMode('dark');
                 } else {
                     document.cookie = 'mode=light; max-age=' + 3600 * 24 * 400;
+                    setMode('light');
                 }
                 router.refresh();
             }}
         >
-            {' '}
-            🌙{' '}
+            {mode === 'light' ? ' 🌙 ' : ' ☀️ '}
         </span>
     );
 }
